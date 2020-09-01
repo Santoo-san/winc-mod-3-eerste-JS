ad9fb5f512c2ec1325bdab9f04f8877eafc1549e
Santoo-san <67684816+Santoo-san@users.noreply.github.com>
2020-09-01 21:28:56 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hello Winc Academy");
+
+let name = "Hans";
+console.log(name);
+
+//opdr 6. variabele met IF kan niet omdat dat een keyword is in JS
+//opdr 7.
+const a = 4;
+console.log(a + a);
+
+let string = "4";
+console.log(string + string); //nee dus
+
+//opdr 8.
+function addition(number) {
+  return number + number;
+}
+console.log(addition(2));
+console.log(a - a);
+console.log(a * a);
+console.log((a + a) / a);
+
+let foo = 5;
+console.log(foo % 2);
+
+//opdr 9.
+let age = 38;
+console.log(typeof age);
+
+//opdr 10.
+let ageTwo = "38";
+console.log(typeof ageTwo);
